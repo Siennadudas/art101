178dc2dd045db26a7e9407e334a1cfd0a18504da
Siennadudas <129894071+Siennadudas@users.noreply.github.com>
2023-05-08 23:42:45 -0700

    lab 8
    added lab 8 index.html page and to homepage

---
+++ lab8/js/lab8.js
@@ -0,0 +1,28 @@
+// index.js - Lab 8: Anon Functions and Callbacks
+// Author: Sienna Dudas
+// Date: 5/8/23
+
+function doubleIt(x) {
+    return 2*x;
+}
+
+// test function
+console.log("11 doubled is:", doubleIt(11));
+console.log("22 doubled is:", doubleIt(22));
+console.log("44 doubled is:", doubleIt(44));
+
+array = [11,22,44,88,176,352,704,1408]
+console.log("My Array", array);
+
+
+var result = array.map(doubleIt);
+//console.log(result);
+
+//var result = array.map(function(x){
+//    return 2*x;
+//})
+
+console.log("Array doubled", result);
+
+
+
