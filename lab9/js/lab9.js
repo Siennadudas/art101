3de785f29acdb18ea7bd17869fcd4e85a0e507fd
Siennadudas <129894071+Siennadudas@users.noreply.github.com>
2023-05-11 00:00:23 -0700

    update lab 9
    added task x

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,14 +4,28 @@
 
 var outputEl = document.getElementById('output');
 
+// create new paragraph element
 var new1El = document.createElement('p');
 
 new1El.innerHTML = "How was your day?";
 
+// create another paragraph element 
 var new2El = document.createElement('p');
 
 new2El.innerHTML = "Goodnight!";
 
+// insert new paragraphs into document
 outputEl.appendChild(new1El);
 
 outputEl.appendChild(new2El);
+
+// create a 3rd element 
+var new3El = document.createElement('p');
+
+// add what the element will display
+new3El.innerHTML = "Hello,";
+
+// insert paragraph into document
+outputEl.prepend(new3El);
+
+
