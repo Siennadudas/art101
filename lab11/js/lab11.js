4778903ef5cfc1bbe6d952752fa92e5e18ae4d9d
Siennadudas <129894071+Siennadudas@users.noreply.github.com>
2023-05-24 22:11:16 -0700

    lab 12
    changes/updates for lab 12

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -3,25 +3,35 @@
 // Date: 5/17/23
 
 $(function() {
-    alert("hello");
+    // opening alert for fun
+    alert("hello, welcome to lab 11.");
+
+    // add button to challenge section
     var challengeButtonHtml = '<button type="button" id="challenge-button"> Go! </button>';
     var challengeButton = $(challengeButtonHtml);
     $("#challengesParagraph").append(challengeButton);
 
+    // add button to problems section
     var problemButtonHtml = '<button type="button" id="problem-button"> Go!! </button>';
     var problemButton = $(problemButtonHtml);
     $("#problemsParagraph").append(problemButton);
 
+    // add button to results section
     var resultsButtonHtml = '<button type="button" id="result-button"> Go!!! </button>';
     var resultsButton = $(resultsButtonHtml);
     $("#myresultsParagraph").append(resultsButton);
 
+    // add event listener to challenge button
     challengeButton.click( function () {
     $("#challengesParagraph").toggleClass("special");
     });
+
+    // add event listener to problems button
      problemButton.click(function () {
     $("#problemsParagraph").toggleClass("special");
     });
+
+    // add event listener to results button
     resultsButton.click(function() {
     $("#myresultsParagraph").toggleClass("special");
     });
---
+++ lab12/js/lab12.js
@@ -0,0 +1,61 @@
+// index.js - Lab 12: Conditonals
+// Author: Sienna Dudas
+// Date: 5/23/23
+
+function sortingHat(str) {
+
+    // count the letters in str
+    var length = str.length;
+
+    // use modulus to get the remainder with 4
+    var mod = length % 4;
+
+    var result = "";
+
+    // return different results depending on value of mod
+    switch(mod) {
+        case 0:
+            result = "Gryffindor";
+            break;
+
+        case 1:
+            result = "Ravenclaw";
+            break;
+
+        case 2:
+            result = "Slytherin";
+            break;
+
+        case 3:
+            result = "Hufflepuff";
+            break;
+    }
+
+    return result;
+}
+
+
+$(function() {
+
+    // find button in document
+    var button = $("#button");
+
+    // add event listener to button
+    button.click(function() {
+
+        // get the value of the form input
+        var name = $("#input").val();
+
+        // send value to sortingHat 
+        var house = sortingHat(name);
+
+        // create paragraph element
+        var outputHtml = '<p class="outputParagraph">The Sorting Hat has sorted you into ' + house + '</p>';
+        var outputParagraph = $(outputHtml);
+
+        // append paragraph to output div
+        $("#output").append(outputParagraph);
+
+    });
+    
+});
