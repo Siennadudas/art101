fd13d445a8e9d8a6838b91bcdce7bfd8389033e4
Siennadudas <129894071+Siennadudas@users.noreply.github.com>
2023-05-26 23:10:14 -0700

    lab 13
    changes to indexd.html for lab 13

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -1,25 +1,34 @@
 function FizzBuzz() {
+        // define constants
+        const multipleOf3 = "Fizz";
+        const multipleOf5 = "Buzz";
+        const multipleof7 = "Pop";
+        const multipleof9 = "RAWR";
 
-
-    // loop through numbers 1 to 200
-    var number = 1;
-    while (number <= 200) {
+        // loop through numbers 1 to 200
+        var number = 1;
+        while (number <= 200) {
 
         var str = "";
 
         // see if number is a multiple of 3
         if (number % 3 == 0) {
-            str += "Fizz";
+            str += multipleOf3;
         }
         
         // see if number is a multiple of 5
         if (number % 5 == 0) {
-            str += "Buzz";
+            str += multipleOf5;
         }
 
         // see if number is a multiple of 7
         if (number % 7 == 0) {
-            str += "Boom";
+            str += multipleOf7;
+        }
+
+        // see if number is a multiple of 9
+        if (number % 9 == 0) {
+            str += multipleOf9;
         }
 
         // add exclamation point to end of string
