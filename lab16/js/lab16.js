d5a7df89c0ffae1a298ada48af352afbd7f7f34c
Siennadudas <129894071+Siennadudas@users.noreply.github.com>
2023-06-09 20:31:14 -0700

    changes per lab 16
    fixing errors

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -1,32 +1,49 @@
 // this function calls xkcd api to get daily comic
 
-function writeOutputToHtml(comicObj) {
-
-    console.log(JSON.stringify(comicObj));
-
-    $("#title").html = comicObj["title"];
-
-    var src = comicObj["img"];
-    var alt = comicObj["alt"];
-
-    $("#image").attr("src", src);
-    $("#image").attr("alt", alt);
-
-}
-
-$(function() {
+function getAndPutData(number) {
 
     // call api with ajax
     $.ajax({
-        url : "https://xkcd.com/info.0.json",
+        url : "https://xkcd.com/" + number + "/info.0.json",
         type : "GET",
         dataType : "json",
         success : function(comicObj) {
-            writeOutputToHtml(comicObj);
+
+            console.log(JSON.stringify(comicObj));
+
+            var title = comicObj["title"];
+            var src = comicObj["img"];
+            var alt = comicObj["alt"];
+
+            $("#title").html = title;
+            $("#image").attr("src", src);
+            $("#image").attr("alt", alt);
         },
+
         error : function(jqXHR, textStatus, errorThrown) {
             console.log("Error:", textStatus, errorThrown);
         }
     });
-    
+}
+
+$(function() {
+
+    var number = 614;
+
+    // add event listener to next button
+    $("#next").click(function() {
+        number = number + 1;
+        getAndPutData(number);
+    });
+
+    // add event listener to previous button
+    $("#previous").click(function() {
+        number = number - 1;
+        getAndPutData(number);
+    });
+
+    getAndPutData(number);
+
 });
+
+
